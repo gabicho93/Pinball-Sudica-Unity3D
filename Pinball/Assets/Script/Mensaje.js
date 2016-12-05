101b30f64856e3133cd66bde577a8a0dbcf02eac
gabicho93 <angabriel50@hotmail.com>
2016-12-04 22:52:35 -0500

    version final del videojuego

---
+++ Pinball/Assets/Script/Mensaje.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var mensaje : String; // mensaje q se va a mostrar 
+var entro : boolean = false;
+
+
+
+
+function Update () {
+
+
+
+
+
+
+}function OnGUI() {
+
+
+    if(entro){
+
+
+        GUI.Label(Rect((Screen.width/2)-40,(Screen.height/2)-100,200,30),mensaje);
+    
+    }
+
+
+
+
+}function OnTriggerEnter() {
+
+
+    entro = true;
+
+
+}function OnTriggerExit() {
+
+
+    entro = false;
+
+
+}
